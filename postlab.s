a016a6c4c5d3cc688538226a3b748cceec95147d
JoseFernandoDeLeon <dele20316@uvg.edu.gt>
2022-02-26 22:10:21 -0600

    Carpeta del Laboratorio #5  completa (incluye prelab, lab y postlab terminados)

---
+++ postlab.s
@@ -0,0 +1,348 @@
+;-------------------------------------------------------------------------------
+;Encabezado
+;-------------------------------------------------------------------------------
+    
+; Archivo: postlab.s
+; Dispositivo: PIC16F887
+; Autor: José Fernando de León González
+; Compilador:  pic-as (v2.30), MPLABX v5.40
+;
+; Programa: displays multiplexados
+; Hardware: Leds y resistencias en el puerto A, pushbuttons en el puerto B, y displays en puerto C.
+;
+; Creado: 7/02/22
+; Última modificación: 7/02/22
+    
+PROCESSOR 16F887
+
+;-------------------------------------------------------------------------------
+;Palabras de configuración 
+;-------------------------------------------------------------------------------
+    
+; CONFIG1
+  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+  CONFIG  PWRTE = ON            ; Power-up Timer Enable bit (PWRT enabled)
+  CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+  CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
+  CONFIG  BOREN = OFF            ; Brown Out Reset Selection bits (BOR enabled)
+  CONFIG  IESO = OFF             ; Internal External Switchover bit (Internal/External Switchover mode is enabled)
+  CONFIG  FCMEN = OFF            ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+  CONFIG  LVP = ON              ; Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+
+; CONFIG2
+  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
+
+;-------------------------------------------------------------------------------
+;Librerías incluidas
+;-------------------------------------------------------------------------------
+  
+#include <xc.inc>
+
+;-------------------------------------------------------------------------------
+;Macros
+;-------------------------------------------------------------------------------
+
+  restart_tmr0 macro
+    BANKSEL TMR0        ; banco 00
+    MOVLW 100            ; cargar valor inicial a W
+    MOVWF TMR0          ; cargar el valor inicial al TIMER0
+    BCF T0IF            ; limpiar la bandera  de overflow del TIMER0
+    endm
+;-------------------------------------------------------------------------------
+;Variables
+;-------------------------------------------------------------------------------
+
+  PSECT udata_shr ; Variables en la memoria RAM compartida entre bancos
+    
+    W_TEMP: DS 1	; 1 byte reservado (W Temporal)
+    STATUS_TEMP: DS 1   ; 1 byte reservado (STATUS Temporal)
+    
+    value:		DS 1	; (Variable que contiene valor a mostrar en los displays de 7-seg)
+    flags:		DS 1	; (Variable que indica que display hay que encender en cada instante)
+    nibbles:		DS 2	; (Variable que divide los nibbles alto y bajo de valor)
+    display_val:	DS 3	; Representación de cada nibble en el display de 7-seg
+
+    centenas:		DS 1	; (Variable para almacenar la cantidad de centenas en el contador)
+    decenas:		DS 1	; (Variable para almacenar la cantidad de decenas en el contador)
+    unidades:		DS 1	; (Variable para almacenar la cantidad de unidades en el contador)
+;-------------------------------------------------------------------------------
+;Vector Reset
+;-------------------------------------------------------------------------------
+
+PSECT VectorReset, class = CODE, abs, delta = 2 ; delta = 2: Las instrucciones necesitan 2 localidades para ejecutarse & abs = absolute: indicamos que contamos a partir de 0x0000
+ORG 00h  ; la localidad del vector reset es 0x0000
+ 
+VectorReset:
+    PAGESEL main
+    GOTO main
+
+;-------------------------------------------------------------------------------
+;Vector de interrupción
+;-------------------------------------------------------------------------------
+ORG 04h	    ; posición 0004h para las interrupciones
+push:
+    MOVWF W_TEMP		;guardamos los valores previos del STATUS y el W en variables temporales
+    SWAPF STATUS, W
+    MOVWF STATUS_TEMP
+isr:
+    
+    BTFSC   RBIF
+    call    int_iocb
+    BTFSC   T0IF
+    call    int_tmr0
+    
+pop:				;regresamos los valores de W y STATUS
+    SWAPF STATUS_TEMP, W
+    MOVWF STATUS
+    SWAPF W_TEMP, F
+    SWAPF W_TEMP, W
+    retfie
+
+;-------------------------------------------------------------------------------
+;Subrutinas de interrupción
+;-------------------------------------------------------------------------------
+    
+int_iocb:
+    banksel PORTB	    ; Vamos al banco del PORTB
+    btfss PORTB, 0	    ; Revisamos si el botón del bit0 de PORTB sigue presionado
+    incf PORTA		    ; NO: Incrementamos PORTA
+    btfss PORTB, 1	    ; Sí: Revisamos si el botón del bit1 de PORTB sigue presionado
+    decf PORTA		    ; NO: Decrementamos PORTA
+    BCF RBIF		    ; SÍ: limpiamos la bandera de interrupción
+    return
+   
+int_tmr0:
+    
+    call display_selection
+    restart_tmr0
+    return
+    
+display_selection:
+
+    BCF	    PORTD, 0		; Apagamos display de nibble alto
+    BCF	    PORTD, 1		; Apagamos display de nibble bajo
+    BCF	    PORTD, 2
+    BTFSC   flags, 1		; Verificamos bandera
+    goto    display_2
+    BTFSC   flags, 0
+    goto    display_0		  
+    goto    display_1
+    
+    return
+    
+display_0:			
+    MOVF    display_val, W	; Movemos display a W
+    MOVWF   PORTC		; Movemos Valor de tabla a PORTC
+    BSF	PORTD, 1	; Encendemos display de nibble bajo
+    BCF	flags, 0	; Cambiamos bandera para cambiar el otro display en la siguiente interrupción
+    BSF flags, 1
+    return
+
+display_1:
+    MOVF    display_val+1, W	; Movemos display+1 a W
+    MOVWF   PORTC		; Movemos Valor de tabla a PORTC
+    BSF	PORTD, 0	; Encendemos display de nibble alto
+    BSF	flags, 0	; Cambiamos bandera para cambiar el otro display en la siguiente interrupción
+    return
+    
+display_2:
+    MOVF    display_val+2, W	; Movemos display+1 a W
+    MOVWF   PORTC		; Movemos Valor de tabla a PORTC
+    BSF	PORTD, 2	; Encendemos display de nibble alto
+    BCF	flags, 0	; Cambiamos bandera para cambiar el otro display en la siguiente interrupción
+    BCF flags, 1
+    
+    return
+;-------------------------------------------------------------------------------
+;Tabla para display de siete segmentos
+;-------------------------------------------------------------------------------
+
+PSECT table, class = CODE, abs, delta = 2
+ORG 100h 
+
+table:
+    CLRF PCLATH
+    BSF PCLATH, 0           ; PCLATH en 01
+    ANDLW 0X0F
+    ADDWF PCL               ; PC = PCLATH + PCL | Sumamos W al PCL para seleccionar un dato en la tabla
+    retlw 00111111B         ; 0
+    retlw 00000110B         ; 1
+    retlw 01011011B         ; 2
+    retlw 01001111B         ; 3
+    retlw 01100110B         ; 4
+    retlw 01101101B         ; 5
+    retlw 01111101B         ; 6
+    retlw 00000111B         ; 7
+    retlw 01111111B         ; 8 
+    retlw 01101111B         ; 9
+    retlw 01110111B         ; A
+    retlw 01111100B         ; b
+    retlw 00111001B         ; C
+    retlw 01011110B         ; D
+    retlw 01111001B         ; C
+    retlw 01110001B         ; F
+
+;-------------------------------------------------------------------------------
+;main (configuración)
+;-------------------------------------------------------------------------------
+main:			    
+    call config_clock	    ; configuramos el reloj 
+    call config_tmr0	    ; configuramos el TIMER0
+    call config_ports	    ; configuramos puertos
+    call config_IOCRB	    ; configuramos las interruptions on change
+    call config_int_enable  ; activamos las interrupciones
+    
+    banksel PORTA
+    			    ;---------------------------------------------------
+			    ;TEMPORIZACIÓN TIMER0: 100 ms = 4*4us*(256-100)*4
+			    ;---------------------------------------------------
+    
+;-------------------------------------------------------------------------------
+;Loop
+;-------------------------------------------------------------------------------
+
+loop:
+    MOVF   PORTA, W		; Valor del PORTA a W
+    MOVWF   value		; Movemos W a variable valor
+    //call    nibble_save		; Guardamos nibble alto y bajo de valor
+    call    display		; Guardamos los valores a enviar en PORTC para mostrar valor en hex
+    
+    CLRF    unidades
+    CLRF    decenas
+    CLRF    centenas
+    
+    call convertir_centenas
+    call convertir_decenas
+    call convertir_unidades
+   
+    goto loop
+;-------------------------------------------------------------------------------
+;subrutinas
+;-------------------------------------------------------------------------------
+config_IOCRB:
+    banksel TRISA
+    BSF IOCB, 0
+    BSF IOCB, 1	    ; seteamos los bits 0 y 1 del puerto B como interrupt on change
+    
+    banksel PORTA
+    MOVF PORTB, W   ; Al leer termina condición de mismatch
+    BCF RBIF
+    
+    return
+config_ports:
+    
+    banksel ANSEL       ; banco 11
+    CLRF ANSEL		; pines digitales
+    CLRF ANSELH
+    
+    banksel TRISA       ; banco 01
+    CLRF TRISA		; PORTA como salida
+    CLRF TRISC		; PORTC como salida
+    CLRF TRISD		; PORTD como salida
+    
+    BSF  TRISB0
+    BSF  TRISB1         ; pines 1 & 2 del puerto B como entradas
+    
+    BCF OPTION_REG, 7	;Habilitar Pull-ups
+    
+    banksel PORTA       ; banco 00
+    CLRF PORTA		; limpiamos PORTA
+    CLRF PORTC		; limpiamos PORTC
+    CLRF PORTD		; limpiamos PORTD
+    CLRF flags
+    return
+    
+config_int_enable:
+    BSF GIE ; INTCON
+    BSF RBIE
+    BCF RBIF
+    
+    BSF T0IE
+    BCF T0IF
+    return
+    
+config_clock:
+    banksel OSCCON      ;banco 01
+    BCF IRCF2
+    BSF IRCF1
+    BCF IRCF0           ; IRCF <2:0> -> 010 250 kHz
+    
+    BSF SCS             ;reloj interno
+    return
+    
+config_tmr0:
+    banksel TRISA  ; banco 01
+    BCF T0CS            ; TIMER0 como temporizador
+    BCF PSA             ; Prescaler a TIMER0
+    BCF PS2
+    BCF PS1
+    BSF PS0             ; PS<2:0> -> preescaler 001 1:4
+    
+    restart_tmr0    
+    return
+/*    
+nibble_save:
+    MOVLW   0x0F		    
+    ANDWF   value, W		; Se hace un AND de value con el valor en W para guardar solo el nibble bajo
+    MOVWF   nibbles		; Guardar el nibble bajo en el primer registro de la variable nibbles	
+				 
+    MOVLW   0xF0		      
+    ANDWF   value, W		; Se hace un AND con el valor en W para guardazr solo el nibble alto	  
+    MOVWF   nibbles+1		; Enviar el valor al segundo registro de la variable nibbles	      
+    SWAPF   nibbles+1, F	; Utilizar un SWAPF para mover los nibbles de su posición high a la posición low	      
+    
+    return
+*/   
+display:
+    MOVF    decenas, W		; Movemos nibble bajo a W
+    call    table		; Buscamos valor a cargar en PORTC
+    MOVWF   display_val		; Guardamos en el primer registro de la variable display_val
+    
+    MOVF    centenas, W		; Movemos nibble alto a W
+    call    table		; Buscamos valor a cargar en PORTC
+    MOVWF   display_val+1	; Guardamos en en el segundo registro de la variable display_val
+    
+    MOVF    unidades, W		; Movemos nibble alto a W
+    call    table		; Buscamos valor a cargar en PORTC
+    MOVWF   display_val+2	; Guardamos en en el segundo registro de la variable display_val
+    
+    return
+    
+convertir_centenas:
+    MOVLW 100			; Enviamos 100 a W
+    SUBWF value, F		; Restamos 100 a w
+    INCF centenas		; incrementamos centenas
+    BTFSC STATUS, 0		; verificamos si se activa el bit de borrow
+    goto $-4			; NO: efectuamos nuevamente las restas
+    DECF centenas		; SÍ: solucionamos problemas de underflow y salimos de la subrutina
+    MOVLW 100
+    ADDWF value, F
+    return
+    
+convertir_decenas:
+    MOVLW 10			; Enviamos 10 a w
+    SUBWF value, F		; Restamos 10 a value
+    INCF decenas		; incrementamos decenas
+    BTFSC STATUS, 0		; verificamos si se activa el bit de borrow
+    goto $-4			; NO: efectuamos nuevamente las restas
+    DECF decenas		; SÍ: solucionamos problemas de underflow y salimos de la subrutina
+    MOVLW 10
+    ADDWF value, F
+    return
+
+convertir_unidades:
+    
+    MOVLW 1			; enviamos 1 a W
+    SUBWF value, F		; restamos 1 a value
+    INCF unidades		; incrementamos unidades
+    BTFSC STATUS, 0		; verificamos si se activa el bit de borrow
+    goto $-4			; NO: efectuamos nuevamente las restas
+    DECF unidades		; SÍ: solucionamos problemas de underflow y salimos de la subrutina
+    MOVLW 1
+    ADDWF value, F
+    return
+
+END
